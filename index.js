609de91b44f1e800b6f1df61433dc7cc49b674e5
Patrick Logan <JPatrickLogan@gmail.com>
2017-02-10 22:55:14 -0700

    hapi assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Created by patricklogan on 2/10/17.
+ */
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
